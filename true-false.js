3a925d523ccc9662c1cb0b301c0b221b78f49eae
rahat2020 <bloodyhell4040@gmail.com>
2021-02-01 22:35:14 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,20 @@
+////this are false. If the parameter value is 0 and others then the parameter value will be false.
+
+// 0 
+// "" => it is a empty string
+// undefined
+// null
+// NaN , false
+
+//this are true. 
+'o' , ' ' , []
+
+let name = 12;
+
+if (name || name ==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
