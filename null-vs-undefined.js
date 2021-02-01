0167ba6d3c738a13ca4adcfe823fc081688a9a7a
rahat2020 <bloodyhell4040@gmail.com>
2021-02-01 23:11:37 +0600

    null vs undefines cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+//Null Vs Undefined, different ways you will get undefined
+
+//undefined number 1
+
+// let paki;
+// function add (num1, num2){
+//     console.log(num1+num2);
+//     return
+// }
+
+// const result = add (12,34);
+// console.log(result);
+
+//undefined number 2
+// function add (num1, num2){
+//     console.log(num1,num2);
+//     return
+// }
+
+// const result = add (34);
+// console.log(result);
+
+//undefined number 3
+// const premik = {name:"akila dananjaya kormo", phone:"908707"};
+// console.log(premik.gf);
+
+//undefined number 4
+let ages = [2,3,4,8];
+console.log(ages[9]);
